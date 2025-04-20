552eb8fc79f8ed5e5b7fe9d105817783f7801d15
willjsw <sunwoo1137@gmail.com>
2025-04-21 01:35:12 +0900

    KW-121/chore: project init

---
+++ src/test/java/com/doubleo/accessservice/AccessServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.doubleo.accessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
