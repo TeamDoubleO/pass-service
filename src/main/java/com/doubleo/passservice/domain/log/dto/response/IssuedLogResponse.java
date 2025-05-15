efcfde3dc056bbd8726741d738a48a4c07aecbcf
coffeesigma <ljb22060@gmail.com>
2025-05-15 14:10:54 +0900

    KW-383/feat: 도메인 재설계 및 그에 따른 dto, service 수정

--- src/main/java/com/doubleo/passservice/domain/log/dto/response/IssuedLogResponse.java
+++ src/main/java/com/doubleo/passservice/domain/log/dto/response/IssuedLogResponse.java
@@ -2,11 +2,14 @@ package com.doubleo.passservice.domain.log.dto.response;
 
 import com.doubleo.passservice.domain.pass.enums.VisitCategory;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record IssuedLogResponse(
         Long memberId,
         String memberName,
         Long passId,
+        String areaCode,
+        List<String> areaName,
         LocalDateTime startAt,
         LocalDateTime expiredAt,
         VisitCategory visitCategory) {}
