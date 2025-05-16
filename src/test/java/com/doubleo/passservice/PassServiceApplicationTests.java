83622e048d2fdec930b2a1a39a3da4ab3a164f40
midday2612 <midday2612@gmail.com>
2025-05-16 17:37:03 +0900

    KW-394/feat: 테스트 시 data.sql 실행 비활성화

--- src/test/java/com/doubleo/passservice/PassServiceApplicationTests.java
+++ src/test/java/com/doubleo/passservice/PassServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.doubleo.passservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"spring.sql.init.mode=never"})
 class PassServiceApplicationTests {
 
     @Test
