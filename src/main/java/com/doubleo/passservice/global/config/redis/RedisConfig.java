c57ef059d1c1b8ac2d99665dc53709b5bf0bf74c
midday2612 <midday2612@gmail.com>
2025-05-21 16:43:59 +0900

    KW-413/KW-412/feat: redis 세팅

---
+++ src/main/java/com/doubleo/passservice/global/config/redis/RedisConfig.java
@@ -0,0 +1,37 @@
+package com.doubleo.passservice.global.config.redis;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(RedisProperties.class)
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfig =
+                new RedisStandaloneConfiguration(redisProperties.host(), redisProperties.port());
+
+        if (!redisProperties.password().isBlank()) {
+            redisStandaloneConfig.setPassword(redisProperties.password());
+        }
+
+        LettuceClientConfiguration lettuceClientConfig =
+                LettuceClientConfiguration.builder()
+                        .commandTimeout(Duration.ofSeconds(1))
+                        .shutdownTimeout(Duration.ZERO)
+                        .build();
+
+        return new LettuceConnectionFactory(redisStandaloneConfig, lettuceClientConfig);
+    }
+}
