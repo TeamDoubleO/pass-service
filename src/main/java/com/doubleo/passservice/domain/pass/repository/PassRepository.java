8694d0cf7951e051dcbbea6a5d44b03559af376d
coffeesigma <ljb22060@gmail.com>
2025-05-26 16:09:50 +0900

    KW-443/feat: 매일 10시 출입증 만료시키는 스케줄러 구현

--- src/main/java/com/doubleo/passservice/domain/pass/repository/PassRepository.java
+++ src/main/java/com/doubleo/passservice/domain/pass/repository/PassRepository.java
@@ -2,6 +2,7 @@ package com.doubleo.passservice.domain.pass.repository;
 
 import com.doubleo.passservice.domain.pass.domain.Pass;
 import com.doubleo.passservice.domain.pass.enums.IssuanceStatus;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,4 +13,7 @@ public interface PassRepository extends JpaRepository<Pass, Long> {
 
     Page<Pass> findAllByTenantIdAndIssuanceStatus(
             String tenantId, IssuanceStatus issuanceStatus, Pageable pageable);
+
+    List<Pass> findAllByExpiredAtBeforeAndIssuanceStatusNot(
+            LocalDateTime now, IssuanceStatus issuanceStatus);
 }
