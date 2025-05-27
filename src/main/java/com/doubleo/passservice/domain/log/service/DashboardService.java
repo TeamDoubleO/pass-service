4ebc2551e1ed167fa051252a09aeedf47ccca696
midday2612 <midday2612@gmail.com>
2025-05-27 16:03:50 +0900

    KW-485/chore: dashboard service, entry stats service 통합

--- src/main/java/com/doubleo/passservice/domain/log/service/DashboardService.java
+++
@@ -1,8 +0,0 @@
-package com.doubleo.passservice.domain.log.service;
-
-import com.doubleo.passservice.domain.log.dto.response.HourlyEntryResponse;
-import java.util.List;
-
-public interface DashboardService {
-    List<HourlyEntryResponse> getHourlyEntryList();
-}
--- src/main/java/com/doubleo/passservice/domain/stats/service/StatsService.java
+++ src/main/java/com/doubleo/passservice/domain/stats/service/StatsService.java
@@ -1,9 +1,12 @@
 package com.doubleo.passservice.domain.stats.service;
 
+import com.doubleo.passservice.domain.log.dto.response.HourlyEntryResponse;
 import com.doubleo.passservice.domain.stats.dto.response.*;
 import java.util.List;
 
 public interface StatsService {
+
+    List<HourlyEntryResponse> getHourlyEntryList();
 
     List<DailyStatsInfoListResponse> getDailyPeriodStatsList();
 
