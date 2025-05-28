950403bcdab60bc115513413a4d6b1ad399f7e9f
Jeongbeen Lee <ljb22060@gmail.com>
2025-05-28 14:24:54 +0900

    [KW-499] feat: grpc error util class 추가, error code registry 추가. (#31)
    * KW-499/feat: grpc error util class 추가, error code registry 추가.
    
    * KW-499/fix: fix error

--- src/main/java/com/doubleo/passservice/global/exception/errorcode/MemberErrorCode.java
+++ src/main/java/com/doubleo/passservice/global/exception/errorcode/MemberErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MemberErrorCode implements BaseErrorCode {
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다.");
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
+    INVALID_REGISTRATION_NUMBER(HttpStatus.BAD_REQUEST, "유효한 주민등록번호가 아닙니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
