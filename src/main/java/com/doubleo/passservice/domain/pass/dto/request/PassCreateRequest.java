17a9675201adc85937a90a3eba0f6b1652cc079d
coffeesigma <ljb22060@gmail.com>
2025-05-31 15:23:48 +0900

    KW-556/fix: update request dto schema

--- src/main/java/com/doubleo/passservice/domain/pass/dto/request/PassCreateRequest.java
+++ src/main/java/com/doubleo/passservice/domain/pass/dto/request/PassCreateRequest.java
@@ -8,4 +8,4 @@ import jakarta.validation.constraints.NotNull;
 public record PassCreateRequest(
         @Schema(description = "방문자 구분", example = "PATIENT") @NotNull VisitCategory visitCategory,
         @Schema(description = "환자 code", example = "A01010") String patientCode,
-        @Schema(description = "시작 시간", example = "2007-12-03T10:15:30") @NotBlank String startAt) {}
+        @Schema(description = "시작 시간", example = "2007-12-03") @NotBlank String startAt) {}
