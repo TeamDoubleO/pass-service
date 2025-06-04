7789c72468fe0666a4efcaaa0f6d3939c2c9475a
coffeesigma <ljb22060@gmail.com>
2025-06-04 13:42:35 +0900

    KW-580/feat: add non null option on dto

--- src/main/java/com/doubleo/passservice/domain/pass/dto/response/MemberPassInfoResponse.java
+++ src/main/java/com/doubleo/passservice/domain/pass/dto/response/MemberPassInfoResponse.java
@@ -3,9 +3,11 @@ package com.doubleo.passservice.domain.pass.dto.response;
 import com.doubleo.passservice.domain.pass.dto.GuardianInfo;
 import com.doubleo.passservice.domain.pass.enums.IssuanceStatus;
 import com.doubleo.passservice.domain.pass.enums.VisitCategory;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record MemberPassInfoResponse(
         Long passId,
         Long memberId,
