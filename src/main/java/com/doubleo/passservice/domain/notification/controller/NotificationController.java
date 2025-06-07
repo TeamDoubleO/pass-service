4733a50e26ffcdd98c72188ebd2402a9a0116526
coffeesigma <ljb22060@gmail.com>
2025-06-07 13:58:37 +0900

    KW-608/feat: add recent member notification get api

--- src/main/java/com/doubleo/passservice/domain/notification/controller/NotificationController.java
+++ src/main/java/com/doubleo/passservice/domain/notification/controller/NotificationController.java
@@ -22,6 +22,15 @@ public class NotificationController {
         return notificationService.getAllMemberNotifications(memberId);
     }
 
+    @Operation(
+            summary = "Recent one Member Notification get API",
+            description = "사용자의 최근 하나의 알림 조회 API")
+    @GetMapping("/recent")
+    public MemberNotificationResponse RecentMemberNotificationGet(
+            @RequestHeader("X-Member-Id") Long memberId) {
+        return notificationService.getRecentMemberNotification(memberId);
+    }
+
     @Operation(summary = "All Member Notifications delete API", description = "모든 사용자의 알림 삭제 API")
     @DeleteMapping
     public ResponseEntity<Void> MemberNotificationListDelete(
