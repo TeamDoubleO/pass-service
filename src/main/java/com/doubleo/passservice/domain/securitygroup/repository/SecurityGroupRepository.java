11a27cce2634504a530970d22d207d4f93052ef4
midday2612 <midday2612@gmail.com>
2025-06-09 11:17:01 +0900

    KW-624/chore/보안그룹 삭제

--- src/main/java/com/doubleo/passservice/domain/securitygroup/repository/SecurityGroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.doubleo.passservice.domain.securitygroup.repository;
-
-import com.doubleo.passservice.domain.securitygroup.domain.SecurityGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SecurityGroupRepository extends JpaRepository<SecurityGroup, Long> {
-    boolean existsByGroupName(String groupName);
-}
