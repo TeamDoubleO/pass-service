acc361ab1da1795db74f2b60c3cb2cde380c03ee
coffeesigma <ljb22060@gmail.com>
2025-06-11 11:17:15 +0900

    change route admin-passes to passes

--- src/main/java/com/doubleo/passservice/domain/pass/controller/AdminPassController.java
+++ src/main/java/com/doubleo/passservice/domain/pass/controller/AdminPassController.java
@@ -12,7 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/admin-passes")
+@RequestMapping("/passes")
 @RequiredArgsConstructor
 public class AdminPassController {
 
