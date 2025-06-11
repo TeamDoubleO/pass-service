5f02a9f6949ada1421123bc16e352b6e0f4921f6
Jeongbeen Lee <ljb22060@gmail.com>
2025-06-11 15:08:16 +0900

    [KW-647] feat: fcm과 알림 저장 로직 분리, 메세지 상세화 (#52)
    * KW-647/feat: fcm과 알림 저장 로직 분리, 메세지 상세화
    
    * KW-647/fix: 오타 수정
    
    * KW-647/fix: 오타 수정

--- src/main/java/com/doubleo/passservice/domain/notification/dto/request/FcmSendRequest.java
+++ src/main/java/com/doubleo/passservice/domain/notification/dto/request/FcmSendRequest.java
@@ -2,10 +2,6 @@ package com.doubleo.passservice.domain.notification.dto.request;
 
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record FcmSendRequest(
-        @NotNull Long memberId,
-        @Nullable String token,
-        @NotBlank String title,
-        @NotBlank String content) {}
+        @Nullable String token, @NotBlank String title, @NotBlank String content) {}
