5e356622fe4f68256e1ddef195c2936daacb7811
Jeongbeen Lee <ljb22060@gmail.com>
2025-06-16 17:20:45 +0900

    Update AcapyClient.java

--- src/main/java/com/doubleo/passservice/grpc/client/AcapyClient.java
+++ src/main/java/com/doubleo/passservice/grpc/client/AcapyClient.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AcapyClient {
 
-    @GrpcClient("didagent-service")
+    @GrpcClient("did-agent")
     private AcapyServiceGrpc.AcapyServiceBlockingStub blockingStub;
 
     public boolean issueVc(String tenantId, Long passId, Long memberId) {
